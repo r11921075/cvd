614ce74238921845e5cfe48123f300e7fae33703
r11921075 <113988402+r11921075@users.noreply.github.com>
2022-12-17 08:56:09 +0800

    123

---
+++ lib/build/CMakeFiles/lib_python.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/content/cvd2/lib/ColorStream.cpp" "CMakeFiles/lib_python.dir/ColorStream.o" "gcc" "CMakeFiles/lib_python.dir/ColorStream.o.d"
+  "/content/cvd2/lib/DepthMapTransform.cpp" "CMakeFiles/lib_python.dir/DepthMapTransform.o" "gcc" "CMakeFiles/lib_python.dir/DepthMapTransform.o.d"
+  "/content/cvd2/lib/DepthPhoto.cpp" "CMakeFiles/lib_python.dir/DepthPhoto.o" "gcc" "CMakeFiles/lib_python.dir/DepthPhoto.o.d"
+  "/content/cvd2/lib/DepthStream.cpp" "CMakeFiles/lib_python.dir/DepthStream.o" "gcc" "CMakeFiles/lib_python.dir/DepthStream.o.d"
+  "/content/cvd2/lib/DepthVideo.cpp" "CMakeFiles/lib_python.dir/DepthVideo.o" "gcc" "CMakeFiles/lib_python.dir/DepthVideo.o.d"
+  "/content/cvd2/lib/FlowConstraints.cpp" "CMakeFiles/lib_python.dir/FlowConstraints.o" "gcc" "CMakeFiles/lib_python.dir/FlowConstraints.o.d"
+  "/content/cvd2/lib/FrameRange.cpp" "CMakeFiles/lib_python.dir/FrameRange.o" "gcc" "CMakeFiles/lib_python.dir/FrameRange.o.d"
+  "/content/cvd2/lib/Importer.cpp" "CMakeFiles/lib_python.dir/Importer.o" "gcc" "CMakeFiles/lib_python.dir/Importer.o.d"
+  "/content/cvd2/lib/Instantiation.cpp" "CMakeFiles/lib_python.dir/Instantiation.o" "gcc" "CMakeFiles/lib_python.dir/Instantiation.o.d"
+  "/content/cvd2/lib/Photo3dIo.cpp" "CMakeFiles/lib_python.dir/Photo3dIo.o" "gcc" "CMakeFiles/lib_python.dir/Photo3dIo.o.d"
+  "/content/cvd2/lib/PoseOptimizer.cpp" "CMakeFiles/lib_python.dir/PoseOptimizer.o" "gcc" "CMakeFiles/lib_python.dir/PoseOptimizer.o.d"
+  "/content/cvd2/lib/Processor.cpp" "CMakeFiles/lib_python.dir/Processor.o" "gcc" "CMakeFiles/lib_python.dir/Processor.o.d"
+  "/content/cvd2/lib/PythonBindings.cpp" "CMakeFiles/lib_python.dir/PythonBindings.o" "gcc" "CMakeFiles/lib_python.dir/PythonBindings.o.d"
+  "/content/cvd2/lib/ValueTransform.cpp" "CMakeFiles/lib_python.dir/ValueTransform.o" "gcc" "CMakeFiles/lib_python.dir/ValueTransform.o.d"
+  "/content/cvd2/lib/core/CvUtil.cpp" "CMakeFiles/lib_python.dir/core/CvUtil.o" "gcc" "CMakeFiles/lib_python.dir/core/CvUtil.o.d"
+  "/content/cvd2/lib/core/Debug.cpp" "CMakeFiles/lib_python.dir/core/Debug.o" "gcc" "CMakeFiles/lib_python.dir/core/Debug.o.d"
+  "/content/cvd2/lib/core/FileIo.cpp" "CMakeFiles/lib_python.dir/core/FileIo.o" "gcc" "CMakeFiles/lib_python.dir/core/FileIo.o.d"
+  "/content/cvd2/lib/core/Logging.cpp" "CMakeFiles/lib_python.dir/core/Logging.o" "gcc" "CMakeFiles/lib_python.dir/core/Logging.o.d"
+  "/content/cvd2/lib/core/MathUtil.cpp" "CMakeFiles/lib_python.dir/core/MathUtil.o" "gcc" "CMakeFiles/lib_python.dir/core/MathUtil.o.d"
+  "/content/cvd2/lib/core/Misc.cpp" "CMakeFiles/lib_python.dir/core/Misc.o" "gcc" "CMakeFiles/lib_python.dir/core/Misc.o.d"
+  "/content/cvd2/lib/core/ParamsBase.cpp" "CMakeFiles/lib_python.dir/core/ParamsBase.o" "gcc" "CMakeFiles/lib_python.dir/core/ParamsBase.o.d"
+  "/content/cvd2/lib/core/Projection.cpp" "CMakeFiles/lib_python.dir/core/Projection.o" "gcc" "CMakeFiles/lib_python.dir/core/Projection.o.d"
+  "/content/cvd2/lib/core/cnpy.cpp" "CMakeFiles/lib_python.dir/core/cnpy.o" "gcc" "CMakeFiles/lib_python.dir/core/cnpy.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/content/cvd2/lib/build/CMakeFiles/lib_cvd.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
